f3f6cd6b739631e67975258e37a6ffbabcf754fc
JulioEvencio <julioigreja.dev@gmail.com>
2024-03-23 18:21:37 -0300

    update: logic game

--- Dodge the Creeps!/scripts/main.gd
+++ Dodge the Creeps!/scripts/main.gd
@@ -14,13 +14,18 @@ func new_game() -> void:
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$EnemyTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("enemies", "queue_free")
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$ScoreTimer.start()
