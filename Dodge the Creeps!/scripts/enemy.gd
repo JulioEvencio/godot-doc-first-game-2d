1b0a13128af01312fd087e167cf83e1a6f7b4e73
JulioEvencio <julioigreja.dev@gmail.com>
2024-03-23 17:33:00 -0300

    added: scene main

--- Dodge the Creeps!/scripts/enemy.gd
+++ Dodge the Creeps!/scripts/enemy.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	var mob_types : PackedStringArray = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
-func _process(_delta) -> void:
+func _process(_delta : float) -> void:
 	pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
---
+++ Dodge the Creeps!/scripts/main.gd
@@ -0,0 +1,45 @@
+extends Node
+
+@export var enemy_scene: PackedScene
+
+var score : int
+
+func _ready() -> void:
+	pass
+
+func _process(_delta : float) -> void:
+	pass
+
+func new_game() -> void:
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$EnemyTimer.stop()
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_start_timer_timeout() -> void:
+	$ScoreTimer.start()
+	$EnemyTimer.start()
+
+func _on_enemy_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+	var enemy_spawn_location = get_node("EnemyPath/EnemySpawnLocation")
+
+	enemy_spawn_location.progress_ratio = randf()
+	
+	var direction = enemy_spawn_location.rotation + PI / 2
+	
+	enemy.position = enemy_spawn_location.position
+	direction += randf_range(-PI / 4, PI / 4)
+	enemy.rotation = direction
+	
+	var velocity : Vector2 = Vector2(randf_range(75.0, 125.0), 0.0)
+	
+	enemy.linear_velocity = velocity.rotated(direction)
+	
+	add_child(enemy)
